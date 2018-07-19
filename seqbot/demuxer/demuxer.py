61df73307e89a35be34803bdbd0ce26064705125
James Webber <james.webber@czbiohub.org>
2018-07-18 18:23:32 -0700

    Added a cache file because querying for fastq folders takes a really long time.

--- seqbot/demuxer/demuxer.py
+++ seqbot/demuxer/demuxer.py
@@ -40,6 +40,7 @@ S3_FASTQS_DIR = 'fastqs'
 
 sample_n = 384
 local_samplesheets = pathlib.Path('/home/seqbot/samplesheets')
+demux_cache = pathlib.Path('/home/seqbot/demux_cached_list.txt')
 
 info_log_file = pathlib.Path('/home/seqbot/flexo_watcher.log')
 debug_log_file = pathlib.Path('/home/seqbot/flexo_debug.log')
@@ -51,7 +52,7 @@ def maybe_exit_process():
 
     cmds = 0
     for pid in pids:
-        with open(os.path.join('/proc', pid, 'cmdline')) as f:
+        with open(pathlib.Path('/proc') / pid / 'cmdline') as f:
             # get the cmdline and match against this script
             line = f.read().split('\x00')[1]
             if line == sys.argv[0]:
@@ -65,7 +66,7 @@ def maybe_exit_process():
         time.sleep(5)
 
 
-def main(logger, fastq_folders, samplesheets):
+def main(logger, demux_set, samplesheets):
     logger.debug("Creating S3 client")
     client = boto3.client('s3')
 
@@ -80,7 +81,7 @@ def main(logger, fastq_folders, samplesheets):
         for fn in fns:
             seq_dir = fn.parent
             seq_base = seq_dir.name
-            if seq_dir.name in fastq_folders:
+            if seq_dir.name in demux_set:
                 logger.debug(f'skipping {seq_dir.name}, already demuxed')
                 continue
 
@@ -92,8 +93,7 @@ def main(logger, fastq_folders, samplesheets):
                     continue
 
                 client.download_file(
-                    Bucket=S3_BUCKET,
-                    Key=os.path.join('sample-sheets', f'{seq_dir.name}.csv'),
+                    Bucket=S3_BUCKET, Key=f'sample-sheets/{seq_dir.name}.csv',
                     Filename=str(local_samplesheets / f'{seq_dir.name}.csv')
                 )
 
@@ -118,7 +118,10 @@ def main(logger, fastq_folders, samplesheets):
                     logger.info(f'demuxing batch {i} of {seq_dir}')
                     subprocess.check_call('reflow run something something')
 
+                demux_set.add(seq_dir.name)
+
     logger.info('scan complete')
+    return demux_set
 
 
 if __name__ == "__main__":
@@ -131,13 +134,19 @@ if __name__ == "__main__":
         (debug_log_file, logging.DEBUG, 'midnight', 3)
     )
 
-    mainlogger.debug('Checking bucket for existing fastq folders')
-    fastq_folders = {
-        fn.split('/', 2)[1] for fn in
-        s3u.get_files(bucket=S3_BUCKET, prefix='fastqs')
-    }
+    if demux_cache.exists():
+        mainlogger.debug('reading cache file for demuxed runs')
+        with open(demux_cache) as f:
+            demux_set = {line.strip() for line in f}
+    else:
+        mainlogger.debug('no cache file exists, querying S3...')
+        demux_set = {
+            fn.split('/', 2)[1] for fn in
+            s3u.get_files(bucket=S3_BUCKET, prefix='fastqs')
+        }
+
     mainlogger.info(
-        f'{len(fastq_folders)} folders in s2://{S3_BUCKET}/fastqs'
+        f'{len(demux_set)} folders in s3://{S3_BUCKET}/fastqs'
     )
 
     mainlogger.debug('Getting the list of sample-sheets')
@@ -149,4 +158,12 @@ if __name__ == "__main__":
         f'{len(samplesheets)} samplesheets in s3://{S3_BUCKET}/sample-sheets'
     )
 
-    main(mainlogger, fastq_folders, samplesheets)
+    updated_demux_set = main(mainlogger, demux_set.copy(), samplesheets)
+
+    mainlogger.info('demuxed {} new runs'.format(
+            len(updated_demux_set) - len(demux_set))
+    )
+    with open(demux_cache, 'w') as OUT:
+        print('\n'.join(sorted(updated_demux_set)), file=OUT)
+
+    mainlogger.debug('wrote new cache file')
